35e7d52216182de106ec52672d272d75faf165fb
davisa94 <austin.i.davis94@gmail.com>
2018-04-24 12:41:42 -0600

    Cannot get it to compile/run problem on line 22 with the while loop condition. If anyone has any idead of how to make it run until it no longer recieves input let me know

--- submit.py
+++ submit.py
@@ -13,4 +13,11 @@ course = 5;
 
 #################################################
 # TODO: read the file to be submitted
+# format test: submit Hello.py /home/burtons/cs241/assign01/.
 #################################################
+garbage = raw_input();
+garbage = raw_input();
+professer_list = [];
+
+while raw_input() != None
+    professer_list.append(str(raw_input()));
