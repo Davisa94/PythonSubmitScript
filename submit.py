c50d3fe0360b3bf9cc3bfd151f3566db6478e964
davisa94 <austin.i.davis94@gmail.com>
2018-04-24 12:29:12 -0600

    Updated the print statment and created constatns for professor and course which may be unneccesarry if we can use a different format with the submit command

--- submit.py
+++ submit.py
@@ -2,4 +2,15 @@ import os;
 import sys;
 fileToSubmit = sys.argv[1];
 
-print "The file to be submitted is: " , str(sys.argv[1]);
+print "The file to be submitted is: " , fileToSubmit;
+
+#################################################
+# Set constants for Brother burtons numbers
+# and the course numbers.
+#################################################
+professor = 36;
+course = 5;
+
+#################################################
+# TODO: read the file to be submitted
+#################################################
